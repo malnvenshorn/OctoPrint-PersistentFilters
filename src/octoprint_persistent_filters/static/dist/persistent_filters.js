43e07e85407df9cae7506b12ea99f887fff7f282
Sven Lohrmann <malnvenshorn@mailbox.org>
2021-10-17 11:24:08 +0200

    Wrap JavaScript in an immediately-invoked function expression
    Prevents naming conflicts if OctoPrint's bundling is disabled

--- src/octoprint_persistent_filters/static/dist/persistent_filters.js
+++ src/octoprint_persistent_filters/static/dist/persistent_filters.js
@@ -1,40 +1,45 @@
-class PersistentFiltersPlugin {
-    constructor(dependencies) {
-        [this.terminal] = dependencies;
+(function () {
+    'use strict';
 
-        // TODO: Should be a static property once this is supported by eslint's parser
-        this.storageKey = 'terminal.activeFilters';
-    }
+    class PersistentFiltersPlugin {
+        constructor(dependencies) {
+            [this.terminal] = dependencies;
 
-    // ViewModel callbacks
-
-    onAfterBinding() {
-        // Do nothing if localSorage is not available
-        if (typeof localStorage === 'undefined') return;
-
-        // Load value from localStorage if set
-        const storedFilters = JSON.parse(localStorage.getItem(this.storageKey));
-        if (storedFilters !== null) {
-            // Delete removed filters
-            const activeFilters = storedFilters.filter((regex) => (
-                this.terminal.filters().some((filter) => filter.regex === regex)
-            ));
-            localStorage.setItem(this.storageKey, JSON.stringify(activeFilters));
-
-            // Apply filters
-            this.terminal.activeFilters(activeFilters);
+            // TODO: Should be a static property once this is supported by eslint's parser
+            this.storageKey = 'terminal.activeFilters';
         }
 
-        // Update localStorage on changes
-        this.terminal.activeFilters.subscribe((newValue) => {
-            localStorage.setItem(this.storageKey, JSON.stringify(newValue));
-        });
-    }
-}
+        // ViewModel callbacks
 
-OCTOPRINT_VIEWMODELS.push({
-    construct: PersistentFiltersPlugin,
-    dependencies: [
-        'terminalViewModel',
-    ],
-});
+        onAfterBinding() {
+            // Do nothing if localSorage is not available
+            if (typeof localStorage === 'undefined') return;
+
+            // Load value from localStorage if set
+            const storedFilters = JSON.parse(localStorage.getItem(this.storageKey));
+            if (storedFilters !== null) {
+                // Delete removed filters
+                const activeFilters = storedFilters.filter((regex) => (
+                    this.terminal.filters().some((filter) => filter.regex === regex)
+                ));
+                localStorage.setItem(this.storageKey, JSON.stringify(activeFilters));
+
+                // Apply filters
+                this.terminal.activeFilters(activeFilters);
+            }
+
+            // Update localStorage on changes
+            this.terminal.activeFilters.subscribe((newValue) => {
+                localStorage.setItem(this.storageKey, JSON.stringify(newValue));
+            });
+        }
+    }
+
+    OCTOPRINT_VIEWMODELS.push({
+        construct: PersistentFiltersPlugin,
+        dependencies: [
+            'terminalViewModel',
+        ],
+    });
+
+})();
